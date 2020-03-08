1adbea2a8aa9e65f909fdc7c65d78cdc581522b1
Gopi Krishna <gopiwayz@gmail.com>
2020-03-08 18:10:55 +0530

    ploished the app

--- src/components/Card.js
+++ src/components/Card.js
@@ -11,34 +11,35 @@ const Card = ({ restaurant }) => {
       <div className='card__content'>
         <div className='card__content_item'>
           <p>
-            <strong>Ramen Variety: </strong>
+            <strong>Ramen Variety:&nbsp;</strong>
             {restaurant['Variety']}
           </p>
         </div>
         <div className='card__content_item'>
           <p>
-            <strong>Style: </strong>
+            <strong>Style:&nbsp;</strong>
             {restaurant['Style']}
           </p>
         </div>
         <div className='card__content_item'>
           <p>
-            <strong>Ramen Country: </strong>
+            <strong>Ramen Country:&nbsp;</strong>
             {restaurant['Country']}
           </p>
         </div>
         <div className='card__content_item'>
           <p>
-            <strong>Rating: </strong>
-            {restaurant['Stars'] !== 'NaN' ? `${restaurant['Stars'] / 5} ⭐` : '-'}
-            
+            <strong>Rating:&nbsp;</strong>
+            {restaurant['Stars'] !== 'NaN'
+              ? `${restaurant['Stars'] / 5} ⭐`
+              : '-'}
           </p>
         </div>
         <div className='card__content_item'>
           <p>
-            <strong>Ranking : </strong>
+            <strong>Ranking :&nbsp;</strong>
             {restaurant['Top Ten'] !== 'NaN'
-              ? `Top  ${restaurant['Top Ten'].split(' ')[1]} in ${
+              ? ` ${restaurant['Top Ten'].split(' ')[1]} in ${
                   restaurant['Top Ten'].split(' ')[0]
                 } `
               : '-'}
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,43 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
-import React from "react";
-import "./Footer.scss";
-
+import React from 'react';
+import './Footer.scss';
 
 const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className='footer'>
       <span>
-        Made with &nbsp;<span style={{ color: "#D1452B" }}>❤️</span> &nbsp; and
+        Made with &nbsp;<span style={{ color: '#D1452B' }}>❤️</span> &nbsp; and
         &nbsp;
-        <span style={{ color: "#61DAFB" }}>⚛️</span>
+        <span style={{ color: '#61DAFB' }}>
+          <a
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            React
+          </a>
+        </span>
       </span>
       &nbsp; | &nbsp;
       <span>
-        Coded By{" "}
+        Coded By{' '}
         <a
-          href="https://bgopikrishna.me"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://bgopikrishna.me'
+          target='_blank'
+          rel='noopener noreferrer'
+          style={{ color: '#f70961' }}
         >
           Gopi Krishna
         </a>
       </span>
       &nbsp; | &nbsp;
       <span>
+        View&nbsp;
         <a
-          href="https://github.com/bgopikrishna/3hr-ramen-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://github.com/bgopikrishna/3hr-ramen-app'
+          target='_blank'
+          rel='noopener noreferrer'
+          style={{ color: '#wheat' }}
         >
           Source Code
         </a>
