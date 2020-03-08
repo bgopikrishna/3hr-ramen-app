4a704e219fb70a0febcd521eddc93236414247b4
Gopi Krishna <gopiwayz@gmail.com>
2020-03-08 16:01:34 +0530

    chore: basic setup completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import { API_ENDPOINT } from './constants';
+import Card from './components/Card';
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      let result = await axios.get(API_ENDPOINT);
+
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {data.map(res => (
+        <Card key={res} restaurant={res}></Card>
+      ))}
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = ({restaurant}) => {
+
+    return (
+        <div>
+            <span>{restaurant.Brand}</span>
+        </div>
+    )
+}
+
+export default Card
